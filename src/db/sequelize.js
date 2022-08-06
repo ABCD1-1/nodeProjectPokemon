1b06e7423630673ae7bb810136d82ff287d6e65b
ABCD1-1 <55496947+ABCD1-1@users.noreply.github.com>
2022-08-06 15:44:24 +0200

    some fix

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,20 +4,35 @@ const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
 const bcrypt = require('bcrypt')
 
-const sequelize = new Sequelize('pokedex', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  dialectOptions: {
-    timezone: 'Etc/GMT-2',
-  },
-  logging: false
-})
+let sequelize
+
+if(process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize('bi73w641is42gtzz', 'l4088pkdvw8s5czo', 'sjwo202li6aeypuq', {
+    host: 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+} else {
+  sequelize = new Sequelize('pokedex', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: false
+  })
+}
+
   
 const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
 
 const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
+  return sequelize.sync().then(_ => {   //.sync({force: true}) to reinitialize the database each time the API Rest is restarted
+    console.log("INIT DB")
     pokemons.map(pokemon => {
       Pokemon.create({
         name: pokemon.name,
